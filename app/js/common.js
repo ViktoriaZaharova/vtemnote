da4057a51cd7e55d373b5a70352e9d11328f546e
viktoriazaharova <zaharova181114@gmail.com>
2020-06-25 20:18:00 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,21 @@
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+$('.go_to').click(function () {
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
+
+$('.panel_heading .block_title').click(function () {
+    $('.panel_heading').removeClass('in');
+    $(this).parents('.panel_heading').addClass('in');
+});
+
