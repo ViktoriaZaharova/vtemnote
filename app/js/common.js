382effb582e0bfc55789cf47ec7647808c91567b
viktoriazaharova <zaharova181114@gmail.com>
2020-06-26 13:50:37 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -19,3 +19,62 @@ $('.panel_heading .block_title').click(function () {
     $(this).parents('.panel_heading').addClass('in');
 });
 
+$('.btn-details').click(function (e) {
+    e.preventDefault();
+    $(this).siblings('.price-box').find('.price-box__hover').fadeIn();
+});
+
+$(window).on('load resize', function() {
+    if ($(window).width() < 1100) {
+        $('.price-slider:not(.slick-initialized)').slick({
+            dots: true,
+            infinite: false,
+            speed: 100,
+            slidesToShow: 1,
+            arrows: true,
+            appendArrows: '.price-wrapper__nav',
+            appendDots: '.price-wrapper__nav',
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>',
+        });
+
+        $('.reviews-slider:not(.slick-initialized)').slick({
+            dots: true,
+            infinite: false,
+            speed: 100,
+            slidesToShow: 2,
+            arrows: true,
+            appendArrows: '.reviews-slider__nav',
+            appendDots: '.reviews-slider__nav',
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>',
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+
+                    }
+                }
+            ]
+        });
+
+        $('.scripts-slider:not(.slick-initialized)').slick({
+            dots: true,
+            infinite: false,
+            speed: 100,
+            slidesToShow: 1,
+            arrows: true,
+            appendArrows: '.scripts-slider__nav',
+            appendDots: '.scripts-slider__nav',
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+            nextArrow: '<button type="button" class="slick-next"></button>',
+        });
+
+    } else {
+        $(".price-slider.slick-initialized").slick("unslick");
+        $(".reviews-slider.slick-initialized").slick("unslick");
+        $(".scripts-slider.slick-initialized").slick("unslick");
+    }
+});
+
